ade793bf64688b98c9ab32dac6dbb6f468040e25
mohammed anwar <moanbm123@gmail.com>
2024-11-21 19:59:57 +0530

    Add Montserrat Italic Variable Font and Update Dependencies
    Added the Montserrat Italic Variable Font to the assets and updated the `pubspec.yaml` and `pubspec.lock` files to include new dependencies such as `flutter_svg`, `http`, `http_parser`, `path_parsing`, `petitparser`, `typed_data`, `vector_graphics`, `vector_graphics_codec`, `vector_graphics_compiler`, `web`, and `xml`. Also, corrected the widget name in `widget_test.dart` to `ResposiveAdptiveDashBoard`.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:responsive_and_adaptive_dashboard/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ResposiveAdptiveDashBoard());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
