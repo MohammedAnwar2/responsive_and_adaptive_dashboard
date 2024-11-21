afce64f793ef1fc4c5959e53ee0be2deb188f6c1
mohammed anwar <moanbm123@gmail.com>
2024-11-21 19:58:04 +0530

    Add SVG images and update app_images.dart with asset paths
    Added multiple SVG images for avatars, balance, card background, dashboard, expenses, income, investments, lekan, logout, settings, statistics, transaction, and wallet account. Updated `app_images.dart` to include constants for these new image assets.

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvator1
+  /// assets/images/avator1.svg
+  static const String imagesAvator1 = "assets/images/avator1.svg";
+
+  /// Assets for imagesAvator2
+  /// assets/images/avator2.svg
+  static const String imagesAvator2 = "assets/images/avator2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/card_background.svg
+  static const String imagesCardBackground = "assets/images/card_background.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestments
+  /// assets/images/investments.svg
+  static const String imagesInvestments = "assets/images/investments.svg";
+
+  /// Assets for imagesLekan
+  /// assets/images/lekan.svg
+  static const String imagesLekan = "assets/images/lekan.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesTransaction
+  /// assets/images/transaction.svg
+  static const String imagesTransaction = "assets/images/transaction.svg";
+
+  /// Assets for imagesWalletAccount
+  /// assets/images/wallet_account.svg
+  static const String imagesWalletAccount = "assets/images/wallet_account.svg";
+}
+
