938c198f033813d551e650993fab578489326529
mohammed anwar <moanbm123@gmail.com>
2024-11-21 19:58:27 +0530

    Add AppColors utility class
    Introduce a new utility class `AppColors` to manage color constants used throughout the application. This class provides a centralized location for defining and accessing color values, ensuring consistency and ease of maintenance.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  AppColors._();
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color lightGray = Color(0xFFE8E8E8);
+  static const Color gray = Color(0xFFB5B5B5); // لون رمادي آخر
+  static const Color darkGray = Color(0xFF292D32);
+  static const Color lightBlue = Color(0xFF4EB7F2);
+  static const Color blue = Color(0xFF208CC8);
+  static const Color aquaBlue = Color(0xFF45AFEA);
+  static const Color lightAquaBlue = Color(0xFF98DAFF);
+  static const Color peach = Color(0xFFF3735E);
+  static const Color lightPeach = Color(0xFFF1F1F1);
+  static const Color veryLightGray = Color(0xFFFAFAFA);
+  static const Color green = Color(0xFF7DD97B);
+  static const Color beige = Color(0xFFE2DECD);
+  static const Color veryLightGray2 = Color(0xFFF7F9FA);
+  static const Color mediumGray = Color(0xFFC4C4C4);
+}
