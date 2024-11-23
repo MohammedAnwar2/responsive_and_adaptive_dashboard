cad191d81bdf42a46cdeb25cd34458bcf392854e
mohammed anwar <moanbm123@gmail.com>
2024-11-23 15:38:08 +0530

    Add AllExpensesItemsModel class
    Introduce a new model class `AllExpensesItemsModel` to represent expense items with properties for image, name, date, and price.

---
+++ lib/models/all_expenses_items_model.dart
@@ -0,0 +1,12 @@
+class AllExpensesItemsModel {
+  final String image;
+  final String name;
+  final String date;
+  final String price;
+
+  const AllExpensesItemsModel(
+      {required this.image,
+      required this.name,
+      required this.date,
+      required this.price});
+}
